2c2cb8c82fe3ff72b81e8f4965eea23a7953b508
monknomo <monknomo@gmail.com>
2017-05-29 15:56:44 -0800

    refactoring with predicates

--- src/main/java/com/gunnargissel/suemez/businessrulerefactorexample/refactor2/BusinessRulesEngine.java
+++ src/main/java/com/gunnargissel/suemez/businessrulerefactorexample/refactor2/BusinessRulesEngine.java
@@ -6,6 +6,7 @@
 package com.gunnargissel.suemez.businessrulerefactorexample.refactor2;
 
 import com.gunnargissel.suemez.businessrulerefactorexample.*;
+import java.util.function.Predicate;
 
 /**
  *
@@ -21,33 +22,41 @@ public class BusinessRulesEngine {
         String category = transfer.getTransferer().getCategory();
         String typeCode = transfer.getTypeCode();
 
+        Predicate<String> isPartner = ttc -> ttc.equals("200");
+        Predicate<String> isFriendsAndFamily = ttc -> ttc.equals("710");
+        Predicate<String> isFriendAndFamilyDiscountLegal = ac -> ac.matches("574|213|363|510");
+        Predicate<String> isPartneringArea = ac -> ac.matches("907|412|213");
+        Predicate<String> isDirigibleArea = ac -> ac.matches("213");
+        Predicate<String> isDirigibleCategory = cat -> cat.equals("D");
+        Predicate<String> isInternal = tc -> tc.equals("I");
+
         if (transfer.getTransferer().getAccount(transfer.getFromAccount()).getBalance().compareTo(transfer.getAmount()) > 0) {
             businessRuleErrors += "Insufficient balance to transfer ; ";
         }
 
-        if (transferTypeCode.equals("200")
-                && !areaCode.matches("907|412|213")) {
+        if (isPartner.test(transferTypeCode)
+                && isPartneringArea.negate().test(areaCode)) {
             businessRuleErrors += "This area is not a transfer eligible area. ; ";
         }
 
-        if (transferTypeCode.equals("200")
-                && areaCode.matches("213")
-                && category.equals("D")) {
+        if (isPartner.test(transferTypeCode)
+                && isDirigibleArea.test(areaCode)
+                && isDirigibleCategory.test(category)) {
             businessRuleErrors += "D Category Transferer can only be transferred in transfer area 213. ; ";
         }
 
-        if (transferTypeCode.equals("710")
-                && !areaCode.matches("574|213|363|510")) {
+        if (isFriendsAndFamily.test(transferTypeCode)
+                && isFriendAndFamilyDiscountLegal.negate().test(areaCode)) {
             businessRuleErrors += "This area is not an eligible area. ; ";
 
         }
 
-        if (!typeCode.equals("I")
+        if (isInternal.negate().test(typeCode)
                 && !isBlockSize(transfer)) {
             businessRuleErrors += "Amount is too small for I type transfer. ; ";
         }
 
-        if (!typeCode.equals("I")
+        if (isInternal.negate().test(typeCode)
                 && isTotalOverCap(transfer)) {
             businessRuleErrors += "This transfer is too large. ; ";
         }
