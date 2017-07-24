1621d589aa3801836fecbd143d4ad262c6d33b25
monknomo <monknomo@gmail.com>
2017-07-24 07:33:36 -0800

    fixed more tests

--- src/main/java/com/gunnargissel/suemez/businessrulerefactorexample/refactor2/BusinessRulesEngine.java
+++ src/main/java/com/gunnargissel/suemez/businessrulerefactorexample/refactor2/BusinessRulesEngine.java
@@ -14,12 +14,12 @@ import java.util.function.Predicate;
  */
 public class BusinessRulesEngine {
 
-    static final Predicate<WidgetTransfer> suffientAmount = trans -> trans.getTransferer().getAccount(trans.getFromAccount()).getBalance().compareTo(trans.getAmount()) > 0;
+    static final Predicate<WidgetTransfer> suffientAmount = trans -> trans.getTransferer().getAccount(trans.getFromAccount()).getBalance().compareTo(trans.getAmount()) < 0;
     static final Predicate<String> isPartner = ttc -> ttc.equals("200");
     static final Predicate<String> isFriendsAndFamily = ttc -> ttc.equals("710");
     static final Predicate<String> isFriendAndFamilyDiscountLegal = ac -> ac.matches("574|213|363|510");
     static final Predicate<String> isPartneringArea = ac -> ac.matches("907|412|213");
-    static final Predicate<String> isDirigibleArea = ac -> ac.matches("213");
+    static final Predicate<String> dirigiblesForbiddenArea = ac -> ac.matches("213");
     static final Predicate<String> isDirigibleCategory = cat -> cat.equals("D");
     static final Predicate<String> isInternal = tc -> tc.equals("I");
 
@@ -41,23 +41,23 @@ public class BusinessRulesEngine {
         }
 
         if (isPartner.test(transferTypeCode)
-                && isDirigibleArea.test(areaCode)
+                && dirigiblesForbiddenArea.negate().test(areaCode)
                 && isDirigibleCategory.test(category)) {
             businessRuleErrors += "D Category Transferer can only be transferred in transfer area 213. ; ";
         }
 
         if (isFriendsAndFamily.test(transferTypeCode)
                 && isFriendAndFamilyDiscountLegal.negate().test(areaCode)) {
-            businessRuleErrors += "This area is not an eligible area. ; ";
+            businessRuleErrors += "This area is not a transfer eligible area. ; ";
 
         }
 
-        if (isInternal.negate().test(typeCode)
-                && !isBlockSize(transfer)) {
+        if (isInternal.test(typeCode)
+                && isBlockSize(transfer)) {
             businessRuleErrors += "Amount is too small for I type transfer. ; ";
         }
 
-        if (isInternal.negate().test(typeCode)
+        if (isInternal.test(typeCode)
                 && isTotalOverCap(transfer)) {
             businessRuleErrors += "This transfer is too large. ; ";
         }
