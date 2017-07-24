43c174e8662c814f98ad9b8607e7dd8bce45a602
monknomo <monknomo@gmail.com>
2017-07-24 07:40:26 -0800

    more tests passing

--- src/main/java/com/gunnargissel/suemez/businessrulerefactorexample/refactor4/BusinessRulesEngine.java
+++ src/main/java/com/gunnargissel/suemez/businessrulerefactorexample/refactor4/BusinessRulesEngine.java
@@ -14,22 +14,22 @@ import java.util.function.Predicate;
  */
 public class BusinessRulesEngine {
 
-    static final Predicate<WidgetTransfer> suffientAmount = trans -> trans.getTransferer().getAccount(trans.getFromAccount()).getBalance().compareTo(trans.getAmount()) > 0;
+    static final Predicate<WidgetTransfer> suffientAmount = trans -> trans.getTransferer().getAccount(trans.getFromAccount()).getBalance().compareTo(trans.getAmount()) < 0;
     static final Predicate<WidgetTransfer> isPartner = trans -> trans.getTransferTypeCode().equals("200");
     static final Predicate<WidgetTransfer> isFriendsAndFamily = trans -> trans.getTransferTypeCode().equals("710");
     static final Predicate<WidgetTransfer> isFriendAndFamilyDiscountLegal = trans -> trans.getAreaCode().matches("574|213|363|510");
     static final Predicate<WidgetTransfer> isPartneringArea = trans -> trans.getAreaCode().matches("907|412|213");
-    static final Predicate<WidgetTransfer> isDirigibleArea = trans -> trans.getAreaCode().matches("213");
+    static final Predicate<WidgetTransfer> isDirigibleForbiddenArea = trans -> trans.getAreaCode().matches("213");
     static final Predicate<WidgetTransfer> isDirigibleCategory = trans -> trans.getTransferer().getCategory().equals("D");
     static final Predicate<WidgetTransfer> isInternal = trans -> trans.getTypeCode().equals("I");
     static final Predicate<WidgetTransfer> isBlockSize = trans -> isBlockSize(trans);
     static final Predicate<WidgetTransfer> isTotalOverCap = trans -> isTotalOverCap(trans);
     
     static final Predicate<WidgetTransfer> parterTransferReqs = trans -> isPartner.and(isPartneringArea.negate()).test(trans);
-    static final Predicate<WidgetTransfer> dirigibleTransferReqs = trans -> isPartner.and(isDirigibleArea).and(isDirigibleCategory).test(trans);
+    static final Predicate<WidgetTransfer> dirigibleTransferReqs = trans -> isPartner.and(isDirigibleForbiddenArea.negate()).and(isDirigibleCategory).test(trans);
     static final Predicate<WidgetTransfer> friendsAndFamilyReqs = trans -> isFriendsAndFamily.and(isFriendAndFamilyDiscountLegal.negate()).test(trans);
-    static final Predicate<WidgetTransfer> internalBlockReqs = trans -> isInternal.negate().and(isBlockSize.negate()).test(trans);
-    static final Predicate<WidgetTransfer> internalTotalCapReqs = trans -> isInternal.negate().and(isTotalOverCap.negate()).test(trans);
+    static final Predicate<WidgetTransfer> internalBlockReqs = trans -> isInternal.and(isBlockSize).test(trans);
+    static final Predicate<WidgetTransfer> internalTotalCapReqs = trans -> isInternal.and(isTotalOverCap).test(trans);
     
     public static final String checkWidgetTransfer(WidgetTransfer transfer) {
         String businessRuleErrors = "";
@@ -47,7 +47,7 @@ public class BusinessRulesEngine {
         }
 
         if (friendsAndFamilyReqs.test(transfer)) {
-            businessRuleErrors += "This area is not an eligible area. ; ";
+            businessRuleErrors += "This area is not a transfer eligible area. ; ";
         }
 
         if (internalBlockReqs.test(transfer)) {
